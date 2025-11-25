edb2b3649906d5a56af67006d42908f9c52b6691
Bot Anica <test.develop.user@gmail.com>
2025-11-25 21:09:29 +0200

    fix get payment_methods request

--- src/app/api/payment-methods/route.ts
+++ src/app/api/payment-methods/route.ts
@@ -2,29 +2,43 @@ import { getBaseUrl } from '@/utils/getBaseUrl';
 import { NextRequest, NextResponse } from 'next/server';
 
 export async function GET(req: NextRequest) {
+  const userIp =
+    req.headers.get("cf-connecting-ip") ||
+    req.headers.get("x-forwarded-for")?.split(",")[0] ||
+    req.headers.get("x-real-ip") ||
+    "0.0.0.0";
+
   try {
-    const baseUrl = getBaseUrl();
+    const apiUrl = getBaseUrl() || 'http://localhost:3001';
     const apiKey = process.env.API_KEY;
 
     if (!apiKey) {
       console.error('API_KEY is not defined in environment variables');
-      return NextResponse.json({ message: 'Internal server error: API_KEY is not configured.' }, { status: 500 });
+      return NextResponse.json({ message: 'Internal server error: API_KEY is missing' }, { status: 500 });
     }
-    
-    const response = await fetch(`${baseUrl}/payment-methods`, {
+
+    const response = await fetch(`${apiUrl}/payment-methods`, {
+      method: 'GET',
       headers: {
+        'Content-Type': 'application/json',
         'x-api-key': apiKey,
+        'x-user-ip': userIp || '',
       },
     });
 
+    const responseData = await response.json();
+
     if (!response.ok) {
-      throw new Error(`HTTP error! status: ${response.status}`);
+      return NextResponse.json(responseData, { status: response.status });
     }
 
-    const data = await response.json();
-    return NextResponse.json(data);
+    return NextResponse.json(responseData);
   } catch (error) {
-    console.error('Failed to fetch payment methods:', error);
-    return NextResponse.json({ message: 'Failed to fetch payment methods' }, { status: 500 });
+    let message = 'Unknown error';
+    if (error instanceof Error) {
+      message = error.message;
+    }
+    return NextResponse.json({ message: `Failed to fetch course tariffs: ${message}` }, { status: 500 });
   }
 }
+
